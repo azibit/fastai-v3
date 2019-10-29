a6edddded3990e0af64b4f61dd20fabfa7f3cd07
Azeez Idris <aidris@Supports-MacBook-Air-3.local>
2019-10-29 00:04:07 -0500

    Update Model Location

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://azibit-models-bucket-1.s3.amazonaws.com/export.pkl'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Resistant', 'Malignant']
 path = Path(__file__).parent
 
 app = Starlette()
